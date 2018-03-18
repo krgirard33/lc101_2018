7339fd11ee1fe36e8475fa0581f21e8ca4428fae
Ken Girard <ken.girard@gmail.com>
2018-03-18 16:41:19 -0500

    tried getting rid of linter errors...

--- hello-flask/main.py
+++ hello-flask/main.py
@@ -1,10 +1,11 @@
-from flask import Flask 
+from flask import Flask
 
 app = Flask(__name__) 
-app.config['DEBUG'] = True 
+app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
+    ''' Hello World '''
     return "Hello World in Flaskish"
 
-app.run() 
+app.run()
