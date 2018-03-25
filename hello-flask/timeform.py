23653cd99d980c2dbdd29b21b48f48698011a3fb
Ken Girard <ken.girard@gmail.com>
2018-03-25 18:07:37 -0500

    Changed timeform over to Jinja2 templates

--- hello-flask/timeform.py
+++ hello-flask/timeform.py
@@ -1,52 +1,22 @@
 from flask import Flask, request, redirect
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__),
+    'templates')
+
+jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(template_dir), autoescape=True
+)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = '''
-<!doctype html>
-<html>
-    <body>
-        <form action="/hello" method="post">
-            <label for="first-name">First Name:</label>
-            <input id="first-name" type="text" name="first_name" />
-            <input type="submit" />
-        </form>
-    </body>
-</html>
-'''
-@app.route("/")
-def index():
-    return form
-
-@app.route('/hello')
-def hello():
-    first_name=request.form['first_name']
-    return '<h1>Hello, ' + first_name +'</h1'
-
-
-time_form = '''
-    <style>
-        .error {{ color: red; }}
-    </style>
-    <h1>Validate Time</h1>
-    <form method='POST'>
-        <label>Hours (24-hour format)
-            <input name="hours" type="text" value='{hours}' />
-        </label>
-        <p class="error">{hours_error}</p>
-        <label>Minutes
-            <input name="minutes" type="text" value='{minutes}' />
-        </label>
-        <p class="error">{minutes_error}</p>
-        <input type="submit" value="Validate" />
-    </form>
-    '''
-
 @app.route('/validate-time')
 def display_time_form():
-    return time_form.format(hours='', hours_error='', 
-        minutes='', minutes_error='')
+    template = jinja_env.get_template('time_form.html')
+    return template.render() 
 
 def is_integer(num):
     try:
@@ -86,11 +56,11 @@ def validate_time():
         return redirect('/valid-time?time={0}'.format(time))
 
     else:
-        return time_form.format(hours_error = hours_error,
+        template = jinja_env.get_template('time_form.html')
+        return template.render(hours_error = hours_error,
             minutes_error = minutes_error, 
             hours = hours,
             minutes = minutes)
-
 
 @app.route('/valid-time')
 def valid_time():
