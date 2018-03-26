7224dcd9bc9f334f3935c40fda001e379cca485e
Ken Girard <ken.girard@gmail.com>
2018-03-25 20:58:10 -0500

    Code along to create a simple tasklist, using python, jinja ifs & fors

---
+++ hello-flask/tasklist.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, redirect
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+tasks=[]
+
+@app.route('/todos', methods=['POST','GET'])
+def todos():
+
+    if request.method == 'POST':
+        task = request.form['task']
+        tasks.append(task)
+
+
+    template = jinja_env.get_template('todos.html')
+    return template.render(title='ToDos',tasks=tasks)
+
+app.run()
