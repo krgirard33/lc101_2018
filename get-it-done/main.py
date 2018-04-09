03207a00cb6d235b134f45040d5975a51145282b
Ken Girard <ken.girard@gmail.com>
2018-04-09 01:28:59 -0500

    Added in Task Class, SQLAlchmey configs

--- get-it-done/main.py
+++ get-it-done/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 import jinja2
 
 '''
@@ -8,6 +9,20 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), a
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+# Note: the connection string after :// contains the following info:
+# user:password@server:portNumber/databaseName
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://gt-it-done:beproductive:8889/get-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
+
+
 
 tasks = []
 
@@ -20,4 +35,7 @@ def index():
 
     return render_template('todos.html', title="Get it Done!", tasks=tasks)
 
-app.run()
+
+
+if __name__ == '__main__':
+    app.run()
