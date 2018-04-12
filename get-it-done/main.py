1848d6dd11bdba4125f00ef2a49227b301a0904e
Ken Girard <ken.girard@gmail.com>
2018-04-12 15:00:13 -0500

    FInished part 9

--- get-it-done/main.py
+++ get-it-done/main.py
@@ -14,15 +14,18 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(120))
     completed = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name):
+    def __init__(self, name, owner):
         self.name = name
         self.completed = False
+        self.owner =  owner
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+    tasks = db.relationship('Task', backref='owner')
 
     def __init__(self, email, password):
         self.email = email
@@ -53,8 +56,8 @@ def register():
             session['email'] = email 
             return redirect('/')
         else:
-            #TODO - user better response messaging
-            return '<h1>Duplicate user</h1>'
+            flash('Email is already registered for user', 'error')
+
     return render_template('register.html')
 
 @app.route('/login', methods=['POST','GET'])
@@ -79,15 +82,15 @@ def logout():
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-
+    owner = User.query.filter_by(email=session['email']).first()
     if request.method == 'POST':
         task_name = request.form['task']
-        new_task = Task(task_name)
+        new_task = Task(task_name, owner)
         db.session.add(new_task)
         db.session.commit()
     
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
+    tasks = Task.query.filter_by(completed=False,owner=owner).all()
+    completed_tasks = Task.query.filter_by(completed=True,owner=owner).all()
     return render_template('todos.html', title="Get it Done!", 
         tasks=tasks, completed_tasks=completed_tasks)
 
