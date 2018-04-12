85604025dfb8d5e0f59adc3027cfe793f5a1ca48
Ken Girard <ken.girard@gmail.com>
2018-04-12 10:53:08 -0500

    Finished part 6

--- get-it-done/main.py
+++ get-it-done/main.py
@@ -18,6 +18,52 @@ class Task(db.Model):
         self.name = name
         self.completed = False
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        #TODO - validate user's data
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            #TODO -'remember' the user 
+            return redirect('/')
+        else:
+            #TODO - user better response messaging
+            return '<h1>Duplicate user</h1>'
+    return render_template('register.html')
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            #TODO -'remember' that user has logged in
+            return redirect('/')
+        else:
+            #TODO - explain why login failed
+            return '<h1>ERROR!</h1>'
+
+    return render_template('login.html')
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
